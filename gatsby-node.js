9b5e6c367a8bc46a32a0f02953f0e95fd16ffb50
Young Hwang <ykcul02@gmail.com>
2021-07-01 22:39:36 +0900

    feat: 블로그 포스트 출력

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,29 @@
 const path = require('path')
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPages } = boundActionCreators;
+  const { createPage } = boundActionCreators
+  const postTemplate = path.resolve('src/templates/blogTemplate')
 
-  const postTemplate = path.resolve('src/tedmplates/blogTemplate')
-}
-
-return graphql(`
-  {
-    allMarkdownRemark {
-      edges {
-        node {
-          frontmatter {
-            path
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
           }
         }
       }
     }
-  }
-`).then(res => {
-  if (res.errors) { return Promise.reject(res.errors) }
+  `).then(res => {
+    if (res.errors) { return Promise.reject(res.errors) }
 
-  res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPages({
-      path: node.frontmatter.path,
-      components: postTemplate
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        components: postTemplate
+      })
     })
   })
-})
+}
--- src/templates/blogTemplate/index.js
+++ src/templates/blogTemplate/index.js
@@ -3,16 +3,18 @@ import { graphql, Link } from "gatsby"
 
 import Layout from "../../components/Layout"
 
-export default function Template({ data }) {
-  const post = data.markdownRemark;
+export default Template = ({ data }) => {
+  const post = data.markdownRemark
   const { title, author, date } = post.frontmatter
 
   return (
     <Layout>
-      <Link to="/">Back to blogs</Link>
-      <h1>{title}</h1>
-      <p>Posted by {author} on {date}</p>
-      <div dangerouslySetInnerHTML={{__html: post.html}} />
+      <div className="blogTemplate">
+        <Link to="/blog">Back to blogs</Link>
+        <h1>{title}</h1>
+        <p>Posted by {author} on {date}</p>
+        <div dangerouslySetInnerHTML={{__html: post.html}} />
+      </div>
     </Layout>
   )
 }

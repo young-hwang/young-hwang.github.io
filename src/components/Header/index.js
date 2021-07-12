ea2468bbb3e7c3d8bb63d0500052e857b94fa74e
hwangyoung <ykcul02@gmail.com>
2021-07-12 16:39:37 +0900

    feat: flex 적용 테스트 및 monitoring query 추가

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,26 +5,26 @@ import { AppBar, Toolbar } from "@material-ui/core"
 
 const Header = () => {
   return (
-    // <AppBar>
-    //   <Toolbar>
-    <nav className="nav">
-      <ul className="nav-list">
-        <li className="nav-list-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="nav-list-item">
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li className="nav-list-item">
-          <Link to="/tags">Tags</Link>
-        </li>
-        <li className="nav-list-item">
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-    // </Toolbar>
-    // </AppBar>
+     <AppBar>
+       <Toolbar>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-list-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-list-item">
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li className="nav-list-item">
+              <Link to="/tags">Tags</Link>
+            </li>
+            <li className="nav-list-item">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </Toolbar>
+    </AppBar>
   )
 }
 
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,20 +1,42 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { CssBaseline } from "@material-ui/core"
-import { ThemeProvider } from "@material-ui/core/styles"
+import { ThemeProvider, makeStyles } from "@material-ui/core/styles"
 
 import Header from "../Header"
 import theme from "../../theme"
 
-const Layout = ({ children }) => (
-  <>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    overflow: 'hidden'
+  },
+  wrapper: {
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    paddingTop: 64
+  }
+}))
+
+const Layout = ({ children }) => {
+  const classes = useStyles()
+
+  return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
-      <main>{ children }</main>
+      <div className={classes.root} >
+        <Header />
+        <div className={classes.wrapper} >
+          <main>{ children }</main>
+        </div>
+      </div>
     </ThemeProvider>
-  </>
-)
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
